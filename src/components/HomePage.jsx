2386da045b33310147eb8b7e750dc0fc37ec0a64
morellaj <alexjamesmorelli@gmail.com>
2020-04-01 03:11:55 -0400

    initial commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,22 @@
 
 // Package dependencies
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 // Component
 export default function HomePage() {
+  const [info, setInfo] = useState(null);
+  useEffect(() => {
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/album/302127")
+      .then((response) => response.json())
+      .then((result) => {
+        setInfo(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div>hi</div>
+    <div>{JSON.stringify(info)}</div>
   );
 }
